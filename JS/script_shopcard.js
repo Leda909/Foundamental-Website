d441500967817109b4c47c492ec7990cf05d76c2
Leda909 <ko9zad@gmail.com>
2023-06-10 22:55:24 +0100

    Colour change by switch case

--- JS/script_shopcard.js
+++ JS/script_shopcard.js
@@ -25,20 +25,50 @@ function Mchange($a){
     
 }
 
-function Cchange($a){
+function Cchange($b){
 
-    if($a=="iPhone 7 Plus"){
-        document.getElementById('Pic').src="images/phones/iP7black.jpg";
-    }
-    else if($a=="iPhone 11"){
-        document.getElementById('Pic').src="images/phones/iP11black.jpg";
-    }
-    else if($a=="iPhone XR"){
-        document.getElementById('Pic').src="images/phones/iPXRblack.jpg";
-    }
-    else {
-        model = $a;
-    }
+    document.getElementById("ColourType").innerHTML= $b;
 
-    
+    if(document.getElementById("ModelType").innerHTML=="iPhone 7 Plus"){
+        switch ($b){
+            case "Black": 
+                document.getElementById("Pic").src="images/phones/iP7black.jpg";
+                break;
+            case "Gold": 
+                document.getElementById("Pic").src="images/phones/iP7gold.jpg";
+                break;
+            case "White":
+                document.getElementById("Pic").src="images/phones/iP7white.jpg";
+                break;
+        }
+    }
+    else if(document.getElementById("ModelType").innerHTML=="iPhone 11")
+	{
+        switch($b) {
+            case "Black":
+                document.getElementById("Pic").src="images/phones/iP11black.jpg";
+                break;
+            case "Gold":
+                document.getElementById("Pic").src="images/phones/iP11gold.jpg";
+                break;
+            case "White":
+                document.getElementById("Pic").src="images/phones/iP11white.jpg";
+                break;				
+        }
+    }
+    else if(document.getElementById("ModelType").innerHTML=="iPhone XR")
+	{
+        switch($b) {
+            case "Black":
+                document.getElementById("Pic").src="images/phones/iPXRblack.jpg";
+                break;
+            case "Gold":
+                document.getElementById("Pic").src="images/phones/iPXRgold.jpg";
+                break;
+            case "White":
+                document.getElementById("Pic").src="images/phones/iPXRwhite.jpg";
+                break;				
+        }
+    }
+    colour = $b; 
 }
