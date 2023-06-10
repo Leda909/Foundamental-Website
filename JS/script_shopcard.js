442039ad6ad232c64c9a28d67334fdaa4c912afe
Leda909 <ko9zad@gmail.com>
2023-06-10 22:29:22 +0100

    add unit price + ModelType

--- JS/script_shopcard.js
+++ JS/script_shopcard.js
@@ -3,6 +3,30 @@ var model, colour = "";
 
 function Mchange($a){
 
+    document.getElementById("ModelType").innerHTML=$a;
+    
+    if($a=="iPhone 7 Plus"){
+        document.getElementById('Pic').src="images/phones/iP7black.jpg";
+        document.getElementById(`Up`).innerHTML="850";
+    }
+    else if($a=="iPhone 11"){
+        document.getElementById('Pic').src="images/phones/iP11black.jpg";
+        document.getElementById(`Up`).innerHTML="1000";
+    }
+    else if($a=="iPhone XR"){
+        document.getElementById('Pic').src="images/phones/iPXRblack.jpg";
+        document.getElementById(`Up`).innerHTML="700";
+    }
+    else {
+      
+        model = $a;
+    }
+
+    
+}
+
+function Cchange($a){
+
     if($a=="iPhone 7 Plus"){
         document.getElementById('Pic').src="images/phones/iP7black.jpg";
     }
