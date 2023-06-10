40fc01f6fd97177b1a451f52144eff2745b6ab14
Leda909 <ko9zad@gmail.com>
2023-06-10 20:14:02 +0100

    text

--- JS/script_slideshow.js
+++ JS/script_slideshow.js
@@ -1,4 +1,9 @@
-texts = [""];
+texts = ["The Self Love mandala.<br>The division of the circle by six, and the green and pink colours is the simbol of the heart chacra. This chackra means love, affirmation, acceptance. The golden orange adds an extra solemn, cheerful mood to the mandala.",
+"The Spiritual Blossoming mandala.<br>In the middle, the six-times repeated flowers and green leafes come into a bouquet, which just shining out from the dark spiritual purple. This dark colour combination gives a mistic feeling to the mandala.",
+"The Affection Pain mandala.<br>It is a six-pionted mandala, with the mixture of smaragd, lihght-purpule and lazac colours. From the centrum, the roundish forms transforms into sharp egges. Those egges simbolizing the pain, and the rest of other are the affirmation.",
+"The Svadhisthana Meditation mandala.<br>Svadhisthana is the third eye's chacra. Their colour is the indigo, purple and blue.",
+"The Chirping Chit-Chat mandala.<br>Twelve happy-yellow birds are chirping on the light-blue area. Actualy, blue is a colour of communication.",
+"The Self-Esteem mandala.<br>Being loved and harmonic with themself, says this mandala, by their roundish forms, smaragdish colours and four-pointed stracture."];
 
 
 titles = ["Self-love","Spiritual blossoming","The affectionate pain","Svadhisthana meditation","Chirping chit-chat","Self-esteem"];
